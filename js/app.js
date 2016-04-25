eb9be777d69824b8c575d7e590c604683c459eeb
Mark <mg8404@my.bristol.ac.uk>
2016-04-26 00:03:51 +0100

    Get root and navbar to render; add watch to dev-deps.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,27 @@
 var app = new Marionette.Application();
 
-app.on('before:start', function() {
-    console.log($('#test'));
-    console.log(Handlebars.templates['testTemplate.hbs']);
+app.on('start', function() {
+
     var RootView = Marionette.LayoutView.extend({
-        template: Handlebars.templates['testTemplate.hbs'],
-        el: 'body'
+
+        template: false,
+        el: 'body',
+        regions: {
+            'nav': '#nav-region',
+            'todo': '#todo-region',
+            'completed': '#completed-region'
+        },
+        initialize: function() {
+
+        },
+
+        onRender: function() {
+            this.showChildView('nav', new NavView());
+        }
     });
+
+    app.root = new RootView();
+    app.root.render();
 });
 
 app.start();
---
+++ js/views/nav.js
@@ -0,0 +1,8 @@
+var NavView = Marionette.ItemView.extend({
+
+    template: Handlebars.templates.nav,
+    initialize: function() {
+        console.log("It worked");
+    }
+
+});
