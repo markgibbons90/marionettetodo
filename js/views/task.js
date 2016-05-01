a6e801254da7e5901302eac0c86abd0ded60f3dc
Mark <mg8404@my.bristol.ac.uk>
2016-05-01 21:16:57 +0100

    Style up task item and 'star' task functionality

--- js/views/task.js
+++ js/views/task.js
@@ -1,5 +1,52 @@
 var TaskView = Marionette.ItemView.extend({
 
     template: Handlebars.templates.task,
+    className: 'task',
+    ui: {
+        titleInput: 'input[type="text"]',
+        starTask: '[data-action="star-task"]',
+        checkTask: '[data-action="check-task"]',
+        taskChecked: '[data-ui="task-checked"]'
+    },
+    events: {
+        'keydown @ui.titleInput': 'onKeyDownInput',
+        'blur @ui.titleInput': 'render',
+        'click @ui.starTask': 'toggleStarred',
+        'click @ui.checkTask': 'markTaskComplete'
+    },
+    modelEvents: {
+        change: 'render'
+    },
+
+    initialize: function() {
+        console.log(this.model.toJSON());
+    },
+
+    /**
+     * When you hit enter in the text input, save the model title
+     * @param  {object} e jQuery event
+     */
+    onKeyDownInput: function(e) {
+        if (e.keyCode === 13) {
+            this.model.set('title', e.target.value);
+        }
+    },
+
+    toggleStarred: function() {
+        var starred = this.model.get('starred');
+        this.model.set('starred', !starred);
+        console.log(this.model.toJSON());
+    },
+
+    markTaskComplete: function() {
+        // Animate checking the task
+        this.ui.checkTask[0].style.display = 'none';
+        this.ui.taskChecked[0].style.display = 'inline-block';
+        // @TODO Set timeout and remove task
+        var _this = this;
+        setTimeout(function() {
+            _this.model.destroy();
+        });
+    }
 
 });
--- js/views/taskList.js
+++ js/views/taskList.js
@@ -3,33 +3,48 @@ var TaskListView = Marionette.CompositeView.extend({
     template: Handlebars.templates.taskList,
     childView: TaskView,
     childViewContainer: '#task-list-container',
+    ui: {
+        taskTextInput: 'input[name="task"]',
+        addTaskButton: '[data-action="add-task"]'
+    },
     events: {
-        'click [data-action="add-task"]': 'addTask',
-        'keydown input[name="task"]': 'onKeyDownInput'
+        'click @ui.addTaskButton': 'addTask',
+        'keydown @ui.taskTextInput': 'onKeyDownInput'
     },
 
     initialize: function() {
-        this.collection = new Backbone.Collection();
+        this.collection = new TaskList();
     },
 
-    onAfterShow: function() {
+    /**
+     * Ensure that the text input is focused on when the view is displayed.
+     */
+    onRender: function() {
         this.$('input[name="task"]').focus();
     },
 
+    /**
+     * When you hit enter in the text input, add the task.
+     * @param  {object} e jQuery event
+     */
     onKeyDownInput: function(e) {
         if (e.keyCode === 13) {
             this.addTask();
         }
     },
 
+    /**
+     * Fetch the task title from the input value and add it to the task collection.
+     */
     addTask: function() {
         var taskTitle = this.$('input[name="task"]').val();
         if (taskTitle.length) {
-            var task = new Backbone.Model({title: taskTitle});
-            this.collection.add(task);
+            var task = new Task({title: taskTitle});
+            this.collection.add(task, {
+                at: 0
+            });
             this.render();
         }
-        this.$('input[name="task"]').focus();
     }
 
 });
